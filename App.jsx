50320e2b48e7324ef7a51512b36a9817f6938650
Manuel Borja <Manuel Borja>
2016-12-14 09:14:32 -0500

    Primeros contenidos

--- App.jsx
+++ App.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 
+var MADRES = [
+{centro:"José Leonardo Ortiz",nombre:"Lucila Campos",parto:"01/01/2017"},
+{centro:"José Leonardo Ortiz",nombre:"Lucila Campos",parto:"01/01/2017"},
+{centro:"José Leonardo Ortiz",nombre:"Lucila Campos",parto:"01/01/2017"},
+{centro:"Chiclayo",nombre:"Lucila Campos",parto:"01/01/2017"},
+{centro:"Chiclayo",nombre:"Lucila Campos",parto:"01/01/2017"},
+{centro:"Lambayeque",nombre:"Lucila Campos",parto:"01/01/2017"},
+{centro:"Lambayeque",nombre:"Lucila Campos",parto:"01/01/2017"},
+{centro:"Lambayeque",nombre:"Lucila Campos",parto:"01/01/2017"},
+{centro:"Lambayeque",nombre:"Lucila Campos",parto:"01/01/2017"}
+]
+
+class MuestraMama extends React.Component{
+    render(){
+         return (
+<div>
+<div>Region</div>
+<div>Centro de salud</div>
+<div>Madre</div>
+<div>Fecha de Parto</div>
+</div>
+         )
+    }
+}
+
 class App extends React.Component {
    render() {
       return (
          <div>
-            Hello World!!!
+            <h1>Mantenimiento de Madres</h1>
+            <MuestraMama madres={MADRES} />
          </div>
       );
    }
--- mama.js
+++
@@ -1,3 +0,0 @@
-class MantenimientoMama extends React.Component{
-   
-}
